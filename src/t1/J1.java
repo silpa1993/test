792d4caa71666047ef7e8507bf0edfd8fc25654c
silpa1993 <mssilpams93@gmail.com>
2019-11-07 21:54:24 +0530

    commiting j1.java hello text

--- src/t1/J1.java
+++ src/t1/J1.java
@@ -5,7 +5,7 @@ public class J1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi");
-System.out.println("Hello");
+System.out.println("Hello text");
 	}
 
 }
